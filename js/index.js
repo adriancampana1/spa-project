4cd3e49e1fe9551777d3b926847aa9ce4d525c46
Adrian Campana <adrianphcampana12@gmail.com>
2023-04-13 13:36:50 -0300

    teste

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-import { Router } from "./router.js";
+import { Router } from './router.js';
 
 const router = new Router();
-router.add("/", "/pages/home.html");
-router.add("/theuniverse", "/pages/theuniverse.html");
-router.add("/exploration", "/pages/exploration.html");
-router.add("/404", "/pages/404.html");
+router.add('/', './pages/home.html');
+router.add('/theuniverse', './pages/theuniverse.html');
+router.add('/exploration', './pages/exploration.html');
+router.add('/404', './pages/404.html');
 
 router.handle();
 
